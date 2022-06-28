e4cc5bd5e30798b4fbb335ea9e03d1d1deeaf026
Lemishevsky Denis <lemishevsky@gmail.com>
2022-06-28 16:07:18 +0300

    fix

--- sender.js
+++ sender.js
@@ -3,7 +3,7 @@ const { CORRECT_FEEDBACK } = require('./_const_start_bot')
 
 require('dotenv').config()
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN_START);
 
 async function sender() {
    const answer = await bot.telegram.sendMessage("", CORRECT_FEEDBACK);
